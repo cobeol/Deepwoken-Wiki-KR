508fcd79924d218514dad8befa66728eb48f26f2
cobeol <cobeol11111010111@gmail.com>
2024-04-15 07:05:48 +0900

    필요없는 파일 제거 + Talents 페이지 Alley Cat까지 번역 완료, 나머지 부분 미리 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,7 @@ const config: Config = {
                     customCss: "./src/css/custom.css",
                 },
                 docs: {
+                    routeBasePath: '/',
                     remarkPlugins: [remarkMath],
                     rehypePlugins: [rehypeKatex],
                 },
